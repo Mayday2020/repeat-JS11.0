0186d2662c57383c43fd1b0840919b51e9d69bf7
Dmitry Likholetov <likholetov1990@gmail.com>
2020-03-28 15:17:03 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
+let money = 30000, 
+    income = 'Фриланс', 
+    addExpenses = 'Бензин, Связь, Коммуналка', 
+    deposit = false, 
+    mission = 300000, 
+    period = 12;
 alert('Любой текст!');
 
 console.log('Также любой текст!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев. Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
