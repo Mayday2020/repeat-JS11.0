8d8c7f3b547394a3b428ea61c8f9646850f01d39
Dmitry Likholetov <likholetov1990@gmail.com>
2020-03-28 18:15:41 +0300

    lesson03

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let money = 30000, 
+'use strict';
+
+let money = prompt("Ваш месячный доход?", 30000), 
     income = 'Фриланс', 
-    addExpenses = 'Бензин, Связь, Коммуналка', 
-    deposit = false, 
+    addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", 
+    'Mobile, Связь, Коммуналка'), 
+    deposit = confirm('Есть ли у вас депозит в банке?'), 
     mission = 300000, 
     period = 12;
-alert('Любой текст!');
 
-console.log('Также любой текст!');
+
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев. Цель заработать ' + mission + ' рублей.');
 console.log(addExpenses.toLowerCase().split(', '));
+console.log(deposit);
 
-let budgetDay = money / 30;
+
+let expenses1,
+    expenses2,
+    amount1,
+    amount2;
+
+let question = () => {
+    expenses1 = prompt('Введите обязательную статью расходов?', 'credit');
+    amount1 = prompt('Во сколько это обойдется?', 2800);
+    expenses2 = prompt('Введите обязательную статью расходов?', 'iqos');
+    amount2 = prompt('Во сколько это обойдется?', 2500);
+};
+question();
+
+let budgetMonth = money - amount1 - amount2;
+console.log('budgetMonth: ', budgetMonth);
+let budgetDay = Math.round(budgetMonth / 30);
 console.log('budgetDay: ', budgetDay);
-
